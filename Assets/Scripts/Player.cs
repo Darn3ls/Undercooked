5fe0beaf762ffc55873a872092e7f8adc1c6423c
Francesco Logli <francesco.logli@yahoo.it>
2023-07-28 17:40:34 +0200

    Walking Animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,11 +18,12 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
-        isWalking = moveDir != Vector3.zero;
-
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+
+        isWalking = moveDir != Vector3.zero;
     }
 
+    //Controllo se il player si sta muovendo (per cambio animazione)
     public bool IsWalking()
     {
         return isWalking;
