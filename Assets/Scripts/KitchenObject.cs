e94371b75c14dd34a7dc04c4735a6a5500ad7929
Francesco Logli <francesco.logli@yahoo.it>
2023-08-10 18:30:27 +0200

    KitchenObject Parent

--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -6,8 +6,29 @@ public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
+    private ClearCounter clearCounter;
+
     public KitchenObjectSO GetKitchenObjectSO()
     {
         return kitchenObjectSO;
+    }
+
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        //REMOVE KitchenObject from old parent
+        if(this.clearCounter != null)
+        this.clearCounter.ClearKitchenObject();
+        
+        //Update the KitchenObject to the new one
+        this.clearCounter = clearCounter;
+        clearCounter.SetKitchenObject(this);
+
+        //Update the visual transform of the KitchenObject
+        transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+    }
+    public ClearCounter GetClearCounter()
+    {
+        return clearCounter;
     }
 }
